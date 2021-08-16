111e1da29985e38218b2966cbe725da564879f5b
danielturmina <dft@cin.ufpe.br>
2021-08-16 14:24:20 -0300

    Inserir Cliente
    Inserir Cliente

--- funcoesInserir.py
+++ funcoesInserir.py
@@ -1,7 +1,35 @@
 import pyodbc
 
-def inserirCliente():
-    return 0
+def inserirCliente(conexao):
+    cpf = input("Digite o CPF do cliente [Ex: 095.038.473-04]: ")
+    nome = input("Digite o nome do cliente: ")
+    sobrenome = input("Digite o sobrenome do cliente: ")
+    cnpj = input("Digite o CNPJ da da emrpesa filiar [Ex: 76.419.484/0001-70]: ")
+    flag= True
+    listaTel = []
+    while flag:
+        telefone = input("Digite um telefone do cliente [Ex: '(81) 98877-6655']: ")
+        teste = input("Deseja inserir mais um telefone para esse cliente ? [s/n]: ")
+        listaTel.append(telefone)
+        if teste == 's':
+            pass
+        else:
+            flag= False
+      
+    inserirCliente = '''INSERT INTO cliente VALUES(?,?,?)'''
+    inserirContrata = '''INSERT INTO contrata VALUES(?,?)'''
+    inserirTelefones = '''INSERT INTO telefones_cliente VALUES(?,?)'''
+    cursor = conexao.cursor()
+    try:
+        cursor.execute(inserirCliente, cpf,nome,sobrenome)
+        cursor.execute(inserirContrata, cnpj, cpf)
+        for x in listaTel:
+            cursor.execute(inserirTelefones, x, cpf)
+        conexao.commit()
+        resultado = "Cliente Inserido com sucesso\n"
+    except:
+        resultado = "Inserção não realizada, você inseriu algum dado inválido\n"
+    return resultado
 
 def inserirAluno():
     return 0
@@ -13,13 +41,13 @@ def inserirEmpregado():
     return 0
 
 def inserirVeiculo(conexao): #Eu queria usar o EXECUTE, e retornar exatamente a mensagem de erro, mas ate agora nao consegui
-    placa = input("Digite a Placa: [Ex: PCU-7705]")
-    categoria = input("Digite a categoria mínima para dirigí-lo: [Ex: A, B, C, D ou E]")
+    placa = input("Digite a Placa [Ex: PCU-7705]: ")
+    categoria = input("Digite a categoria mínima para dirigí-lo [Ex: A, B, C, D ou E]: ")
     modelo = input("Digite o modelo do veículo: ")
     qnt_lugares = int(input("Digite a quantidade máxima de lugares do veículo: "))
-    cnh_motorista = input("Digiteo num da CNH do motorista: [Ex: 60768950437]")
-    cpf_motorista = input("Digite o CPF do motorista: [Ex: 095.038.473-04]")
-    cnpj_filial = input("Digite o CNPJ da da emrpesa filiar: [Ex: 76.419.484/0001-70]")
+    cnh_motorista = input("Digiteo num da CNH do motorista [Ex: 60768950437]: ")
+    cpf_motorista = input("Digite o CPF do motorista [Ex: 095.038.473-04]: ")
+    cnpj_filial = input("Digite o CNPJ da da emrpesa filial [Ex: 76.419.484/0001-70]: ")
     
     inserir = '''INSERT INTO veiculo VALUES(?,?,?,?,?,?,?)'''
     cursor = conexao.cursor()
--- projeto.py
+++ projeto.py
@@ -17,7 +17,11 @@ while inicio:
     conexao = retornar_conexão()
     if acao == 1:
         acao2 = int(input('O que você deseja fazer?\n Digite 1 - Para inserir novo cliente\n Digite 2 - Para inserir novo aluno\n Digite 3 - Para inserir nova escola\n Digite 4 - Para inserir novo empregado\n Digite 5 - Para inserir novo veículo\n Digite 6 - Para voltar ao menu anterior\n Digite sua escolha: '))
-        if acao2 == 5:
+        if acao2 == 1:
+            inserir = ins.inserirCliente(conexao)
+            print(inserir)
+            pass
+        elif acao2 == 5:
             inserir = ins.inserirVeiculo(conexao)
             print(inserir)
             pass
